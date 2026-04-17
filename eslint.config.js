9d61ef1df103af3b4ae2d0f81751ad5aeb4d71a3
Arnab Banik <arnab.banik@optimizely.com>
2026-04-18 02:59:42 +0600

    (eslint): update rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,28 @@
 //  @ts-check
 
-import { tanstackConfig } from '@tanstack/eslint-config'
+import { tanstackConfig } from '@tanstack/eslint-config';
 
 export default [
   ...tanstackConfig,
   {
     rules: {
+      curly: ['error', 'all'],
       'import/no-cycle': 'off',
-      'import/order': 'off',
-      'sort-imports': 'off',
+      'import/order': 'error',
+      'sort-imports': 'error',
       '@typescript-eslint/array-type': 'off',
-      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/require-await': 'warn',
       'pnpm/json-enforce-catalog': 'off',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'react-hooks/exhaustive-deps': 'error',
     },
   },
   {
-    ignores: ['eslint.config.js', 'prettier.config.js'],
+    ignores: [
+      'eslint.config.js',
+      'prettier.config.js',
+      ' **/node_modules/**',
+      'src/routeTree.gen.ts',
+    ],
   },
-]
+];
