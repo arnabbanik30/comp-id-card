61f6c2205ca441d95d94223cde48b9d6d84b21f8
Arnab Banik <arnab.banik@optimizely.com>
2026-04-26 03:31:33 +0600

    (id-card): add a pattern bg in the id card

--- src/components/ui/id-card/Whole.tsx
+++ src/components/ui/id-card/Whole.tsx
@@ -5,7 +5,7 @@ type props = {
 };
 export function Whole({ children }: props) {
   return (
-    <div className="w-[102mm] h-[145mm] bg-white shadow-2xl border border-gray-400 flex flex-col overflow-hidden relative font-sans ">
+    <div className="w-[102mm] h-[145mm] bg-white crosshatch-art-light-bg shadow-2xl border border-gray-400 flex flex-col overflow-hidden relative font-sans ">
       {children}
     </div>
   );
